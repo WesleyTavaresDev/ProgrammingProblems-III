83d582d771e7ae176fd52601bb3857b8c22a0648
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-11 03:59:40 -0300

    Sum class concluded

--- src/com/arrays/Sum.java
+++ src/com/arrays/Sum.java
@@ -1,6 +1,18 @@
 package src.com.arrays;
 
+
+//1.Write a Java program to sum values of an array.
 public class Sum
 {
-       
+    public static void main(String[] args)
+    {
+        int[] values = {1, 2, 3, 4};
+
+        int sum = 0;
+        for(int v : values)
+            sum += v;
+        
+
+        System.out.printf("Sum of arrays values: %s \n", sum);
+    }
 }
