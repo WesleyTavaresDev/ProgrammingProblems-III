5284782a510eee1a6c133171cefd9c9027bab4ca
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-10 23:56:48 -0300

    For loop  extracted to an own method

--- src/com/conditional/GreatestNumber.java
+++ src/com/conditional/GreatestNumber.java
@@ -1,37 +1,44 @@
 package src.com.conditional;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 //2. Take three numbers from the user and print the greatest number.
 public class GreatestNumber 
 {
     public static void main(String[] args)
     {
-        int[] numbers = new int[3];
-
+        List<Integer> numbers = new ArrayList<Integer>(3);
+        
         System.out.println("Please, enter three numbers: ");
         Scanner sc = new Scanner(System.in);
-    
-        for(int i = 0; i < numbers.length; i++)
-        {
-            numbers[i] = sc.nextInt();
-        }
+
+        addElements(numbers, sc);
+
 
         int greatest = 0;
-        if(numbers[0] > numbers[1] && numbers[0] > numbers[2])
+
+        if(numbers.get(0) > numbers.get(1) && numbers.get(0) > numbers.get(2))
         {
-            greatest = numbers[0];
+            greatest = numbers.get(0);
         }
 
-        else if(numbers[1] > numbers[2])
+        else if(numbers.get(1) > numbers.get(2))
         {
-            greatest = numbers[1];
+            greatest = numbers.get(1);
         }
 
         else
         {
-            greatest = numbers[2];
+            greatest = numbers.get(2);
         }
 
-        System.out.println(greatest + "is the greatest number");
+        System.out.println(greatest + " is the greatest number");
+    }
+
+    private static void addElements(List<Integer> numbers, Scanner sc) {
+        numbers.add(sc.nextInt());                
+        numbers.add(sc.nextInt());        
+        numbers.add(sc.nextInt());
     }
 }
