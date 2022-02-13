bbc90fbacd3d32bfeb144c27ed73b4ced81033f9
WesleyTavDev <91393560+WesleyTavaresGameDev@users.noreply.github.com>
2022-02-12 22:49:27 -0300

    Main class created

---
+++ src/com/oop/person/Main.java
@@ -0,0 +1,11 @@
+package com.oop.person;
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        Person newton = new Person(25, 01, 2006, "Newton");
+        
+        System.out.println(newton.calculateAge());
+    }    
+}
--- src/com/oop/person/Person.java
+++ src/com/oop/person/Person.java
@@ -3,38 +3,37 @@ package com.oop.person;
 import java.time.LocalDate;
 import java.util.Calendar;
 /**
- * TODO -> Create a constructor;
- * TODO -> Create test with Junit5;
+ * //TODO -> Create a constructor;
+ * //TODO -> Create a main class;
+ * TODO -> Create test cases with Junit5;
  * TODO -> Commit;
  * TODO -> Start to refact;
  */
 public class Person 
 {
-    int age;
     int day;
     int month;
     int yearOfBirth;
     String name;
   
-    public Person(int age, int day, int month, int year, String name)
+    public Person(int day, int month, int year, String name)
     {
-        this.age = age;
         this.day = day;
         this.month = month;
         this.yearOfBirth = year;
         this.name = name;
     }
 
-    static int calculateAge(int day, int month, int year)
+    public int calculateAge()
     {
         LocalDate date = LocalDate.now();
         Calendar today = Calendar.getInstance();
 
         if(today.get(Calendar.DAY_OF_MONTH) >= day && date.getMonthValue() == month || date.getMonthValue() > month)
         {
-            return date.getYear() - year;
+            return date.getYear() - yearOfBirth;
         }
         else
-            return date.getYear() - year - 1;
+            return date.getYear() - yearOfBirth - 1;
     }
 }
