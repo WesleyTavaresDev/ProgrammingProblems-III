b69f0044a3b3841a424974d05ef9aad0fd595b64
Wesley <wesleyTavaresDev@outlook.com>
2022-02-15 15:25:11 -0300

    Age calculator class created

--- src/com/oop/football/Player.java
+++ src/com/oop/football/Player.java
@@ -1,9 +1,9 @@
 package src.com.oop.football;
 
+import src.com.oop.football.Position.positions;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import src.com.oop.football.Position.positions;
 
 @AllArgsConstructor
 @Getter
@@ -12,8 +12,29 @@ public class Player
 {
     private String name;
     private positions position;
-    private String birthDay;
+
+    private int dayBirth, monthBirth, yearBirth;
+
     private String nationality;
-    private int height;
+    private double height;
     private int weight;
+
+
+    public static void main(String[] args)
+    {
+        Player botafogo = new Player("Wesley", positions.Defense ,25, 06, 2006, "Brazilian", 1.72D, 60);
+        botafogo.playerInfo();
+        
+    }
+
+    public void playerInfo()
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Player position: " + position);
+
+        System.out.printf("\n%s/%s/%s\n", dayBirth, monthBirth, yearBirth);
+        System.out.println("Nationality: " + nationality);
+        System.out.println("Height:"  + height);
+        System.out.println("Weight: " + weight);
+    }
 }
