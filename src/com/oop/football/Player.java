1bd62b3f00cc287b671182eebacd0423f89a559a
Wesley <wesleyTavaresDev@outlook.com>
2022-02-15 11:42:02 -0300

    Player -> construtor fields -> name;                position;                birthDay;                nationality;                height;                weight.

--- src/com/oop/football/Player.java
+++ src/com/oop/football/Player.java
@@ -1,6 +1,15 @@
 package src.com.oop.football;
 
+import lombok.AllArgsConstructor;
+import src.com.oop.football.Position.positions;
+
+@AllArgsConstructor
 public class Player
 {
-    
+    private String name;
+    private positions position;
+    private String birthDay;
+    private String nationality;
+    private int height;
+    private int weight;
 }
