42ab09cb510be93a3e42f469f5524ba86c8c5ec8
Wesley <wesleyTavaresDev@outlook.com>
2022-02-15 11:34:06 -0300

    Position -> positions enum added. Each value of enum is the time to retire

--- src/com/oop/football/Position.java
+++ src/com/oop/football/Position.java
@@ -3,4 +3,15 @@ package src.com.oop.football;
 public class Position 
 {
     
+    public enum positions 
+    {
+        Defense(40), Midfield(38), Attackers(35);
+        int value;
+        private positions(int value)
+        {
+            this.value = value;
+        }
+    };
+
+
 }
