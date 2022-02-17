2346bd001778591d51bdbaa93ea1075bb4eeca5e
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-17 18:43:59 -0300

    Main class instantiate  Employee object and call employee methods

--- src/com/oop/employee/Main.java
+++ src/com/oop/employee/Main.java
@@ -2,5 +2,16 @@ package src.com.oop.employee;
 
 public class Main 
 {
-    
+   public static void main(String[] args) 
+   {
+       Employee employee = new Employee(400, 8);
+
+       System.out.printf("Salary now -> %s %n", employee.getSalary());
+       
+       employee.addSal();
+       System.out.printf("Salary now -> %s %n", employee.getSalary());
+   
+        employee.addWork();
+        System.out.printf("Salary now -> %s %n", employee.getSalary());
+    } 
 }
