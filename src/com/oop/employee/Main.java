6624e8c2b49465a3d2332a6d513de659adee9547
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-17 18:30:32 -0300

    Getters added

---
+++ src/com/oop/employee/Main.java
@@ -0,0 +1,11 @@
+package src.com.oop.employee;
+
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        Employee employee = new Employee(400, 8);
+
+        
+    }    
+}
