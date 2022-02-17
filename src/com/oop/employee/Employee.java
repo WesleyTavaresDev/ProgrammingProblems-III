f83f22513c35aba235dac5c5b9673136c6a193d3
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-17 18:26:13 -0300

    addWork Method created

--- src/com/oop/employee/Employee.java
+++ src/com/oop/employee/Employee.java
@@ -5,15 +5,21 @@ public class Employee
     private double salary = 0.0d;
     private int hoursOfWork = 0;     
 
-    public Employee(double salary, int hoursOfWork)
+    public Employee(double salary, int hoursOfWorkPerDay)
     {
         this.salary = salary;
-        this.hoursOfWork = hoursOfWork;
+        this.hoursOfWork = hoursOfWorkPerDay;
     }
 
     public void addSal()
     {
         if(salary < 500)
             salary += 10;
+    }
+
+    public void addWork()
+    {
+        if(hoursOfWork > 6)
+            salary += 5;
     }
 }
