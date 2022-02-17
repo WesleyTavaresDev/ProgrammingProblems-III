e390dd59708bf7e5cf41fe53bfce0eb2693ef2d0
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-17 18:22:01 -0300

    Employee constructor

--- src/com/oop/employee/Employee.java
+++ src/com/oop/employee/Employee.java
@@ -2,5 +2,12 @@ package src.com.oop.employee;
 
 public class Employee 
 {
+    private double salary = 0.0d;
+    private int hoursOfWork = 0;     
 
+    public Employee(double salary, int hoursOfWork)
+    {
+        this.salary = salary;
+        this.hoursOfWork = hoursOfWork;
+    }
 }
