7b8f01b5de9ad286dae422235d7b445cdd039ae0
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-19 11:12:30 -0300

    ConcatenateSorted concluded

--- src/com/list/concatenate/sort/ConcatenateSorted.java
+++ src/com/list/concatenate/sort/ConcatenateSorted.java
@@ -1,5 +1,21 @@
 package src.com.list.concatenate.sort;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
-public class ConcatenateSorted {
+public class ConcatenateSorted <T>
+{
+    public static void main(String[] args) 
+    {
+        System.out.println(concatAndSort(Arrays.asList(5,6,8), Arrays.asList(4, 7, 9)));
+    }  
     
+    public static <T> List<T> concatAndSort(List<T> base, List<T> other)
+    {
+        List<T> concatenatedList = Stream.concat(base.stream(), other.stream()).sorted().toList();
+        
+        return concatenatedList;
+    }
+
 }
