1b7e6c2b750af2f56e374c582134cea2aa23680c
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-19 09:21:51 -0300

    Sum of out list elements with while loop

--- src/com/list/sum/SumNumbers.java
+++ src/com/list/sum/SumNumbers.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * TODO ->  for-loop 
+ * //TODO ->  for-loop 
  * TODO ->  while-loop and
  * TODO ->  recursion.
  */
@@ -13,7 +13,9 @@ public class SumNumbers
 {
     public static void main(String[] args) 
     {
-       System.out.println("Sum with for-loop -> " + forLoopSum( Arrays.asList(1, 2, 3, 4, 5, 6)));
+        System.out.println("Sum with for-loop -> " + forLoopSum( Arrays.asList(1, 2, 3, 4, 5, 6)));
+        
+        System.out.println("Sum with while-loop -> " + whileLoopSum(Arrays.asList(1, 2, 3, 4, 5)));
     }  
 
     public static int forLoopSum(List<Integer> numbers)
@@ -21,6 +23,19 @@ public class SumNumbers
         int sum = 0;
         for(Integer n : numbers)
             sum += n;
+        return sum;
+    }
+
+    public static int whileLoopSum(List<Integer> numbers)
+    {
+        int i = 0;
+        int sum = 0;
+        while(i < numbers.size())
+        {
+            sum += numbers.get(i);
+            i++;
+        }
+
         return sum;
     }
 
