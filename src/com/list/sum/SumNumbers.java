533052d7f1133944ae1e2182a5135486fce6390b
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-19 09:10:09 -0300

    Sum of our list elements with for-loop

--- src/com/list/sum/SumNumbers.java
+++ src/com/list/sum/SumNumbers.java
@@ -1,6 +1,27 @@
 package src.com.list.sum;
 
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * TODO ->  for-loop 
+ * TODO ->  while-loop and
+ * TODO ->  recursion.
+ */
+
 public class SumNumbers
 {
-        
+    public static void main(String[] args) 
+    {
+       System.out.println("Sum with for-loop -> " + forLoopSum( Arrays.asList(1, 2, 3, 4, 5, 6)));
+    }  
+
+    private static int forLoopSum(List<Integer> numbers)
+    {
+        int sum = 0;
+        for(Integer n : numbers)
+            sum += n;
+        return sum;
+    }
+
 }
