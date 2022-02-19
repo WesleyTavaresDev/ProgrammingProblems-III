f2a5341fbb0efc2fd8cd1d85010cba3b78df5e55
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-02-19 10:37:59 -0300

    Concat two list concluded

--- src/com/list/concatenate/ListConcatenate.java
+++ src/com/list/concatenate/ListConcatenate.java
@@ -1,6 +1,22 @@
 package src.com.list.concatenate;
 
-public class ListConcatenate 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ListConcatenate <T>
 {
-        
-}
+    public static void main(String[] args) 
+    {
+        System.out.println(concat(Arrays.asList("a", "b", "c"), Arrays.asList(1, 2, 3)));
+
+        System.out.println(concat(Arrays.asList("Brenda", "Wesley", "Luiza"), Arrays.asList(19, 16, 5)));
+    }
+
+    public static <T> List<T> concat(List<T> base, List<T> other)
+    {
+        List<T> concatenatedList = Stream.concat(base.stream(), other.stream())
+            .toList();
+        return concatenatedList;
+    }
+} 
